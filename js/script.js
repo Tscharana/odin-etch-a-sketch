6a1d8c5d35ac344f9846c1de5606c274d2c931f0
Tscharana <171447830+Tscharana@users.noreply.github.com>
2025-12-11 02:10:40 +0100

    Create hover effect and cell class

--- js/script.js
+++ js/script.js
@@ -2,9 +2,16 @@ const container = document.querySelector("#container");
 
 for (i = 1; i <= 256; i++) {
     const newCell = document.createElement("div");
+    newCell.classList.add("cell");
     const dimension = "20";
     newCell.style.width = `${dimension}px`;
     newCell.style.height = `${dimension}px`;
     newCell.style.border = "solid 2px red";
-    container.appendChild(newCell);
+        container.appendChild(newCell);
 }
+
+container.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("cell")) {
+        e.target.style.backgroundColor = "grey";
+    }
+})
