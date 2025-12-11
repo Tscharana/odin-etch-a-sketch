fb3445cd253bf9d5afc28df386e0a91813c2002f
Tscharana <171447830+Tscharana@users.noreply.github.com>
2025-12-11 02:33:35 +0100

    Create two buttons and link createGrid() and removeGrid() functions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
 const container = document.querySelector("#container");
 
-for (i = 1; i <= 256; i++) {
-    const newCell = document.createElement("div");
-    newCell.classList.add("cell");
-    const dimension = "20";
-    newCell.style.width = `${dimension}px`;
-    newCell.style.height = `${dimension}px`;
-    newCell.style.border = "solid 2px red";
+function createGrid () {
+    for (i = 1; i <= 256; i++) {
+        const newCell = document.createElement("div");
+        newCell.classList.add("cell");
+        const dimension = "20";
+        newCell.style.width = `${dimension}px`;
+        newCell.style.height = `${dimension}px`;
+        newCell.style.border = "solid 2px red";
         container.appendChild(newCell);
+    }
+}
+
+function removeGrid () {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 }
 
 container.addEventListener("mouseover", (e) => {
     if (e.target.classList.contains("cell")) {
         e.target.style.backgroundColor = "grey";
     }
+})
+
+const creator = document.querySelector("#createGridButton");
+const remover = document.querySelector("#removeGridButton");
+
+creator.addEventListener("click", () => {
+    createGrid();
+})
+
+remover.addEventListener("click", () => {
+    removeGrid();
 })
